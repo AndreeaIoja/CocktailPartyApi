c141985da06afdf1806c79d5127e3fe8908e89fa
Ioja Andreea <ioja_andreea1998@yahoo.com>
2026-01-09 15:41:41 +0200

    initial commit with basic infrastructure of the project

---
+++ DataAccess/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Ingredient> Ingredients => Set<Ingredient>();
+    }
+}
