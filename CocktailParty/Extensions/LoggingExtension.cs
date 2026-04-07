fe7f6c5631e50cf6ed68c42d7e8126b35356d7a2
Ioja Andreea <ioja_andreea1998@yahoo.com>
2026-04-07 10:38:44 +0300

    adds a basic logging system

---
+++ CocktailParty/Extensions/LoggingExtension.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Serilog;
+
+namespace CocktailParty.Extensions
+{
+    public static class LoggingExtension
+    {
+        public static WebApplicationBuilder AddCustomLogging(this WebApplicationBuilder builder)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File(
+                    "logs/api-.log",
+                    rollingInterval: RollingInterval.Day,
+                    retainedFileCountLimit: 14)
+                .CreateLogger();
+
+            builder.Host.UseSerilog();
+
+            return builder;
+        }
+
+        public static WebApplication UseGlobalExceptionHandling(this WebApplication app)
+        {
+            app.UseExceptionHandler(errorApp =>
+            {
+                errorApp.Run(async context =>
+                {
+                    var exceptionFeature = context.Features
+                        .Get<IExceptionHandlerFeature>();
+
+                    if (exceptionFeature != null)
+                    {
+                        Log.Error(
+                            exceptionFeature.Error,
+                            "Unhandled exception occurred");
+                    }
+
+                    context.Response.StatusCode = 500;
+                    context.Response.ContentType = "application/json";
+
+                    await context.Response.WriteAsJsonAsync(new
+                    {
+                        message = "Internal server error"
+                    });
+                });
+            });
+
+            return app;
+
+        }
+
+    }
+}
